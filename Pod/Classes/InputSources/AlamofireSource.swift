421aa58fe5c0cf353dda1024e8a5b4cfbb86ce49
Chainan Satayapiwat <chainan.s@dreamhatch.co.th>
2016-05-09 00:05:57 +0700

    Allow to change Alamofire Manager
    Allow to change Alamofire Manager on its input source

--- Pod/Classes/InputSources/AlamofireSource.swift
+++ Pod/Classes/InputSources/AlamofireSource.swift
@@ -10,14 +10,19 @@ import Alamofire
 import AlamofireImage
 
 public class AlamofireSource: NSObject, InputSource {
-    var url: NSURL!
+    public static var sharedManager = Alamofire.Manager.sharedInstance
     
-    public init(url: NSURL) {
+    var url: NSURL!
+    var sManager: Manager?
+    
+    public init(url: NSURL, sessionManager: Manager? = nil) {
         self.url = url
+        sManager = sessionManager
         super.init()
     }
 
-    public init?(urlString: String) {
+    public init?(urlString: String, sessionManager: Manager? = nil) {
+        self.sManager = sessionManager
         if let validUrl = NSURL(string: urlString) {
             self.url = validUrl
             super.init()
@@ -28,7 +33,11 @@ public class AlamofireSource: NSObject, InputSource {
     }
     
     public func setToImageView(imageView: UIImageView) {
-        Alamofire.request(.GET, self.url)
+        if sManager == nil {
+            sManager = AlamofireSource.sharedManager
+        }
+        
+        sManager!.request(.GET, self.url)
             .responseImage { response in
                 if let image = response.result.value {
                     imageView.image = image
